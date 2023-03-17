1e96cef87d44c788a7833e885daaa5b1900eaacd
Dhirax Darakhe <dhirajdarakhe03@gmail.com>
2023-03-17 16:41:13 +0530

    don

---
+++ CPP/12new_heap_memo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+// HII
+// this is dsqure
+
+int main()
+{
+    int *num;
+    num = new int(3);
+    for (int i = 0; i < 3; ++i)
+        cin >> *(num + i);
+    cout << endl;
+    for (int i = 0; i < 3; ++i)
+        cout << *(num + i) << " ";
+    return 0;
+    // in 1 2 3
+    // out 1 2 3
+}
