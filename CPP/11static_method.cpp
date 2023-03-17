0369062ef7093b89c9ad55a48af3abc3c4788851
Dhirax Darakhe <dhirajdarakhe03@gmail.com>
2023-03-17 16:43:05 +0530

    don

--- CPP/11static_method.cpp
+++ CPP/11static_method.cpp
@@ -1,116 +1,41 @@
-class Solution
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+// HII
+// this is dsqure
+class Test
 {
 public:
-    int dtod(int x, int y, int colu) { return (x * colu) + y; }
-    void f(vector<vector<string>> &ans, vector<string> &ds, int n, int r, vector<vector<pair<int, int>>> &changes_i_made)
+    // 1
+    // static int v = 0;
+
+    // 2
+    static int v;
+
+    Test()
     {
-        if (r == n)
-        {
-            int ans1 = 0;
-            for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j)
-                    if (ds[i][j] == '*')
-                        ans1++;
-            if (ans1 == n)
-                ans.push_back(ds);
-            return;
-        }
-
-        for (int i = 0; i < n; ++i)
-        {
-            if (ds[r][i] == '.')
-            {
-                ans[r][i] = '*';
-                int R = r;
-                int C = i;
-
-                R--;
-                while (R >= 0)
-                {
-                    if (ds[R][C] == '.')
-                    {
-                        changes_i_made[dtod(r, i, n)].push_back({R, C});
-                        ds[R][C] = '*';
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    R--;
-                }
-                R = r;
-                C = i;
-                R++;
-                while (R < n)
-                {
-
-                    if (ds[R][C] == '.')
-                    {
-                        changes_i_made[dtod(r, i, n)].push_back({R, C});
-                        ds[R][C] = '*';
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    R++;
-                }
-                R = r;
-                C = i;
-                C++;
-                while (C < n)
-                {
-
-                    if (ds[R][C] == '.')
-                    {
-                        changes_i_made[dtod(r, i, n)].push_back({R, C});
-                        ds[R][C] = '*';
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    C++;
-                }
-                R = r;
-                C = i;
-                C--;
-                while (C >= 0)
-                {
-
-                    if (ds[R][C] == '.')
-                    {
-                        changes_i_made[dtod(r, i, n)].push_back({R, C});
-                        ds[R][C] = '*';
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    C--;
-                }
-                R = r;
-                C = i;
-                f(ans, ds, n, R + 1, changes_i_made);
-                for (auto x : changes_i_made[dtod(r, i, n)])
-                {
-                    ds[x.first][x.second] = '.';
-                }
-            }
-        }
-    }
-
-    vector<vector<string>> solveNQueens(int n)
-    {
-        vector<vector<string>> ans;
-        vector<string> ds;
-        vector<vector<pair<int, int>>> changes_i_made(n * n, vector<pair<int, int>>());
-        for (int j = 0; j < n; ++j)
-        {
-            string s(n, '.');
-            ds.push_back(s);
-        }
-        f(ans, ds, n, 0, changes_i_made);
-        return ans;
+        v++;
+        // cout << v << " ";
     }
 };
+
+// 2
+int Test ::v = 0;
+
+int main()
+{
+    // 1
+    // Test t1;
+    // Test t2;
+    // 10static.cpp:9:20: error: ISO C++ forbids in-class initialization of non-const static member 'Test::v'
+    //  static int v = 0;
+
+    // 2
+    Test t1;
+    Test t2;
+    // 1 2
+    t1.v = 12;
+    t2.v = 1123;
+    cout << Test ::v << " " << t1.v << "  " << t2.v << endl;
+    return 0;
+}
